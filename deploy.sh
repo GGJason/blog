864248a57db9110c9e3646a925d2c0f8fe0333fb
軒竹 吳 <jasonwu@webim.com.tw>
2020-11-30 03:24:05 +0800

    Fix CI Error

--- deploy.sh
+++ deploy.sh
@@ -15,8 +15,16 @@ echo 'ggjason.tw' > CNAME
 
 
 # Setup Git 
-git config --global user.email "ggjason.tw@gmail.com"
-git config --global user.name "GGJason"
+
+INPUT_EMAIL=${INPUT_EMAIL:-'github-action@users.noreply.github.com'}
+INPUT_NAME=${INPUT_NAME:-'GitHub Action'}
+INPUT_ACTOR=${INPUT_ACTOR:-${GITHUB_ACTOR}}
+INPUT_TOKEN=${INPUT_TOKEN:-"${{ secrets.GITHUB_TOKEN }}"}
+
+git config user.email ${INPUT_EMAIL}
+git config user.name ${INPUT_NAME}
+git config user.password ${INPUT_TOKEN}
+echo "GIT_USER=${INPUT_ACTOR}:${INPUT_TOKEN}" >> $GITHUB_ENV
 
 git init
 git add -A
