2834f916313131fda78fed982d99bb027bbc39a1
軒竹 吳 <jasonwu@webim.com.tw>
2020-11-30 02:45:27 +0800

    Fix CI Error

--- deploy.sh
+++ deploy.sh
@@ -13,12 +13,17 @@ cd blog/.vuepress/dist
 # if you are deploying to a custom domain
 echo 'ggjason.tw' > CNAME
 
+# Git Setup
+git config --global user.email "ggjason.tw@gmail.com"
+git config --global user.name "GGJason"
+
 git init
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:GGJason/blog.git master:gh-pages
