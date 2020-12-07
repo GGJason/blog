e31fd3382f895b0e6e31086b09053d84b5c7dfb1
軒竹 吳 <jasonwu@webim.com.tw>
2020-12-08 02:04:11 +0800

    try fix ci

--- deploy.sh
+++ deploy.sh
@@ -29,6 +29,6 @@ git commit -m 'deploy'
 
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:GGJason/blog.git master:gh-pages
+git push -f https://github.com/GGJason/blog.git master:gh-pages
 
 cd -
